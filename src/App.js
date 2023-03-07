d624cce44c9c9a72c8421d27087a5ea2d5551c6a
Gurman deep <gurman@Gurmans-MacBook-Pro.local>
2023-03-07 19:24:56 +0530

    Day 2 debouncing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
 import Pagination from './Pagination/Body'
+import Search from './Debouncing/Search'
 function App() {
   return (
     <div className="App">
-     <Pagination/>
+     <Search/>
     </div>
   );
 }
